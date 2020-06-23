d12176ace7448716953720e4dad0ee4d05c6f706
Lewis <ofili@users.noreply.github.com>
2020-06-23 22:32:47 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chat
+
+This project creates a server and client(s) for simple terminal-based chatting using WebSocket (golang.org/x/net/websocket).
---
+++ server/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	_ "github.com/gorilla/mux"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/net/websocket"
+)
+
+// Message ...
+type Message struct {
+	Text string `json:"text"`
+}
+
+type hub struct {
+	clients				map[string]*websocket.Conn
+	addClientChan		chan *websocket.Conn
+	removeClientChan	chan *websocket.Conn
+	broadcastChan		chan Message
+}
+
+var (
+	port = flag.String("port", "9000", "port used for ws connection")
+)
+
+//server func will look like hhtp server, with routing
+func server(port string) error {
+	h := newHub()
+	mux := http.NewServeMux()
+	mux.Handle("/", websocket.Handler(func(ws *websocket.Conn) {
+		handler(ws, h)
+	}))
+
+	s := http.Server{Addr: ":" + port, Handler: mux}
+	return s.ListenAndServe()
+}
+
+func handler(ws *websocket.Conn, h *hub)  {
+	go h.run()
+
+	h.addClientChan <- ws
+
+	for {
+		var m Message
+		err := websocket.JSON.Receive(ws, &m)
+		if err != nil {
+			h.broadcastChan <- Message{err.Error()}
+			h.removeClient(ws)
+			return
+		}
+		h.broadcastChan <- m
+	}
+}
+
+func newHub() *hub {
+	return &hub{
+		clients: make(map[string]*websocket.Conn),
+		addClientChan: make(chan *websocket.Conn),
+		removeClientChan: make(chan *websocket.Conn),
+		broadcastChan: make(chan Message),
+	}
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case conn := <-h.addClientChan:
+			h.addClient(conn)
+		case conn := <-h.removeClientChan:
+			h.removeClient(conn)
+		case m := <-h.broadcastChan:
+			h.broadcastMessage(m)
+		}
+	}
+}
+
+// Methods called in run. The addClient and removeClient add and remove a conn from
+// the pool (called clients in the struct) respectively
+// The brodcastMessage method calls websocket's JSON.Send()
+func (h *hub) removeClient (conn *websocket.Conn) {
+	delete(h.clients, conn.LocalAddr().String())
+}
+
+func (h *hub) addClient (conn *websocket.Conn) {
+	h.clients[conn.RemoteAddr().String()] = conn
+}
+
+func (h *hub) broadcastMessage(m Message) {
+	for _, conn := range h.clients {
+		err := websocket.JSON.Send(conn, m)
+		if err != nil {
+			fmt.Println("Error broadcasting message: ", err)
+			return
+		}
+	}
+}
+
+func main()  {
+	flag.Parse()
+	log.Fatal(server(*port))
+}
